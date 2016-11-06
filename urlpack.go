55825a3be7e01cd67d620cfbde2d22a351d80ae6
IlyaKlimov <iklimov89@gmail.com>
2016-11-06 17:44:46 +0000

    Add tests

--- urlpack.go
+++ urlpack.go
@@ -1,62 +1,65 @@
 package urlpack
 
 import (
-    "fmt"
-    // "os"
     "net/url"
     "strings"
     "strconv"
-    // "encoding/json"
     "encoding/hex"
     "crypto/sha256"
     "errors"
 )
 
 type URL struct {
-    hash string
-    string string
-    scheme string
-    authority Authority
-    path Path
-    query Query
-    fragment string
+    Hash        string      `json:"hash"`
+    String      string      `json:"string"`
+    Scheme      string      `json:"scheme"`
+    Authority   Authority   `json:"authority"`
+    Path        Path        `json:"path"`
+    Query       Query       `json:"query"`
+    Fragment    string      `json:"fragment"`
 }
 
 type Authority struct {
-    userinfo *url.Userinfo
-    host Host
+    Userinfo    Userinfo    `json:"userinfo"`
+    Host        Host        `json:"host"`
+}
+
+type Userinfo struct {
+    Username    string      `json:"username"`
+    Password    string      `json:"password"`
+    String      string      `json:"string"`
 }
 
 type Host struct {
-    string string
-    hostname Hostname
-    port int
+    String      string      `json:"string"`
+    Hostname    Hostname    `json:"hostname"`
+    Port        int         `json:"port"`
 }
 
 type Hostname struct {
-    FQDN string
-    Domains []string
-    DomainsCount int
-    WWW bool
-    TLD string
+    FQDN            string      `json:"fqdn"`
+    Domains         []string    `json:"domains"`
+    DomainsCount    int         `json:"domainsCount"`
+    WWW             bool        `json:"www"`
+    TLD             string      `json:"tld"`
 }
 
 type Path struct {
-    string string
-    Dir []string
-    file File
+    String  string      `json:"string"`
+    Dir     []string    `json:"dir"`
+    File    File        `json:"file"`
 }
 
 type File struct {
-    Fullname string
-    Name string
-    Ext []string
+    Fullname    string      `json:"fullname"`
+    Name        string      `json:"name"`
+    Ext         []string    `json:"ext"`
 }
 
 type Query struct {
-    string string
-    list map[string][]string
-    Delimiter string
+    String      string                  `json:"string"`
+    List        map[string][]string     `json:"list"`
+    Delimiter   string                  `json:"delimiter"`
 }
 
 
@@ -70,6 +73,10 @@ func New(urlStr string) (URL, error) {
         return urlPack, err
     }
 
+    if u.Path == "" {
+        u.Path = "/"
+    }
+
     u.Scheme = strings.ToLower(u.Scheme)
     u.Host = strings.ToLower(u.Host)
 
@@ -81,11 +88,11 @@ func New(urlStr string) (URL, error) {
         return urlPack, err
     }
 
-    if err = urlPack.Path().setFields(); err != nil {
+    if err = urlPack.Path.setFields(); err != nil {
         return urlPack, err
     }
 
-    if err = urlPack.Query().setDelimiter(); err != nil {
+    if err = urlPack.Query.setDelimiter(); err != nil {
         return urlPack, err
     }
 
@@ -95,203 +102,100 @@ func New(urlStr string) (URL, error) {
 
 }
 
-func (urlPack *URL) Hash() string {
-    return urlPack.hash
-}
-
-func (urlPack *URL) String() string {
-    return urlPack.string
-}
-
-func (urlPack *URL) Userinfo() *url.Userinfo {
-    return urlPack.authority.userinfo
+func (urlPack *URL) Userinfo() *Userinfo {
+    return &urlPack.Authority.Userinfo
 }
 
 func (urlPack *URL) Host() *Host {
-    return &urlPack.authority.host
+    return &urlPack.Authority.Host
 }
 
 func (urlPack *URL) Hostname() *Hostname {
-    return &urlPack.authority.host.hostname
+    return &urlPack.Authority.Host.Hostname
 }
 
-func (urlPack *URL) Path() *Path {
-    return &urlPack.path
+func (urlPack *URL) File() *File {
+    return &urlPack.Path.File
 }
-
-func (urlPack *URL) Query() *Query {
-    return &urlPack.query
-}
-
-func (q *Query) String() string {
-    return q.string
-}
-
-// func (urlPack *URL) JSON() ([]byte, error) {
-
-// }
-
 
 func (urlPack *URL) setStandardFields(u *url.URL) error {
     var err error
-    urlPack.string = u.String()
-    urlPack.scheme = u.Scheme
-    urlPack.authority.userinfo = u.User
-    urlPack.authority.host.string = u.Host
-    urlPack.path.string = u.Path
-    urlPack.query.string = u.RawQuery
-    urlPack.fragment = u.Fragment
-    urlPack.query.list = u.Query()
+    urlPack.String = u.String()
+    urlPack.Scheme = u.Scheme
+    if u.User != nil {
+        urlPack.Authority.Userinfo.String = u.User.String()
+        urlPack.Authority.Userinfo.Username = u.User.Username()
+        urlPack.Authority.Userinfo.Password, _ = u.User.Password()
+    }
+    urlPack.Authority.Host.String = u.Host
+    urlPack.Path.String = u.Path
+    urlPack.Query.String = u.RawQuery
+    urlPack.Fragment = u.Fragment
+    urlPack.Query.List = u.Query()
     return err
 }
 
 func (urlPack *URL) setHash() {
     h := sha256.New()
-    b := []byte(urlPack.String())
+    b := []byte(urlPack.String)
     h.Write(b)
-    urlPack.hash = hex.EncodeToString(h.Sum(nil))
+    urlPack.Hash = hex.EncodeToString(h.Sum(nil))
 }
 
 func (h *Host) setFields() error {
     var err error
-    host := strings.Split(h.string, ":")
+    host := strings.Split(h.String, ":")
     if len(host) == 2 {
-        if h.port, err = strconv.Atoi(host[1]); err != nil {
+        if h.Port, err = strconv.Atoi(host[1]); err != nil {
             return err
         }
     } else {
-        h.port = 80
+        h.Port = 80
     }
-    h.hostname.FQDN = host[0]
-    h.hostname.Domains = strings.Split(h.hostname.FQDN, ".")
-    h.hostname.DomainsCount = len(h.hostname.Domains)
-    h.hostname.TLD = h.hostname.Domains[h.hostname.DomainsCount-1]
-    if h.hostname.Domains[0] == "www" {
-        h.hostname.WWW = true
+    h.Hostname.FQDN = host[0]
+    h.Hostname.Domains = strings.Split(h.Hostname.FQDN, ".")
+    h.Hostname.DomainsCount = len(h.Hostname.Domains)
+    h.Hostname.TLD = h.Hostname.Domains[h.Hostname.DomainsCount-1]
+    if h.Hostname.Domains[0] == "www" {
+        h.Hostname.WWW = true
     }
     return err
 }
 
 func (p *Path) setFields() error {
     var err error
-    pathStr := strings.Trim(p.string, "/")
-    if p.string == "" {
+    pathStr := strings.Trim(p.String, "/")
+    if pathStr == "" {
         return err
     }
+    pathStr = strings.Replace(pathStr, "//", "/", -1)
     path := strings.Split(pathStr, "/")
     pathLastKey := len(path) - 1
     if strings.ContainsAny(path[pathLastKey], ".") {
-        p.file.Fullname = path[pathLastKey]
-        file := strings.Split(p.file.Fullname, ".")
+        p.File.Fullname = path[pathLastKey]
+        file := strings.Split(p.File.Fullname, ".")
         if len(file) < 2 {
             return errors.New("Bad filename in URL path!")
         }
-        p.file.Name = file[0]
-        p.file.Ext = file[1:]
+        p.File.Name = file[0]
+        p.File.Ext = file[1:]
         p.Dir = path[:pathLastKey]
     } else {
         p.Dir = path
     }
-
-    fmt.Println(p.Dir)
     return err
 }
 
 func (q *Query) setDelimiter() error {
     var err error
-    if q.string == "" {
+    if q.String == "" {
         return err
     }
     switch {
-        case strings.ContainsAny(q.string, "&"):
+        case strings.ContainsAny(q.String, "&"):
             q.Delimiter = "&"
-        case strings.ContainsAny(q.string, ";"):
+        case strings.ContainsAny(q.String, ";"):
             q.Delimiter = ";"
-        default:
-            return errors.New("Bad delimiter in URL query!")
     }
     return err
 }
-
-
-
-
-
-
-
-
-// type URL struct {
-//         Scheme     string
-//         Opaque     string    // encoded opaque data
-//         User       *Userinfo // username and password information
-//         Host       string    // host or host:port
-//         Path       string
-//         RawPath    string // encoded path hint (Go 1.5 and later only; see EscapedPath method)
-//         ForceQuery bool   // append a query ('?') even if RawQuery is empty
-//         RawQuery   string // encoded query values, without '?'
-//         Fragment   string // fragment for references, without '#'
-// }
-
-// func main() {
-//  var urlStr string = "http://user:pass@www.Example.com:80/dir1/dir2/index.php?q1=hello&q2=world"
-
-//  fmt.Println(urlStr)
-
-//  u, err := url.Parse(urlStr)
-//  if err != nil {
-//      fmt.Println(err)
-//  }
-
-//  fmt.Println("Scheme: ", u.Scheme)
-//  fmt.Println("Opaque: ", u.Opaque)
-//  fmt.Println("User: ", u.User)
-//  fmt.Println("Host: ", u.Host)
-//  fmt.Println("Path: ", u.Path)
-//  fmt.Println("RawQuery: ", u.RawQuery)
-//  fmt.Println("Fragment: ", u.Fragment)
-//  fmt.Println("User Password: ", u.User.Username())
-
-//  pass, b :=  u.User.Password()
-
-//  fmt.Println("User Password: ", pass)
-//  fmt.Println("User Password (b): ", b)
-
-//  fmt.Println("IsAbs: ", u.IsAbs())
-
-//  fmt.Println("u: ", u)
-
-//  fmt.Println("u query: ", u.Query())
-
-//  u.Scheme = strings.ToLower(u.Scheme)
-//  u.Host = strings.ToLower(u.Host)
-
-//  fmt.Println("u: ", u)
-
-//  urlStr2 := u.String()
-
-//  fmt.Println(urlStr2)
-
-//  var us URL
-//  fmt.Println(us.string)
-
-
-
-
-// }
-
-/*
-    domainStr := "www.api.ya.example.com"
-    domain := strings.Split(domainStr, ".")
-    fmt.Println(domain)
-
-    domainCount := len(domain)
-
-    fmt.Println(domainCount)
-
-    tld := domain[domainCount-1]
-
-    fmt.Println(tld)
-
-
-
-*/
